bf8157513b1f45dc62e4cdf0603c99c3b0bfe644
batrawi <batrawi2000forever@gmail.com>
2024-11-21 07:44:13 +0200

    setup jumping logic , free fall logic

--- dapper_dasher.cpp
+++ dapper_dasher.cpp
@@ -12,6 +12,10 @@ int main ()
   const int rectangleHeight {50};
   int rectanglePosX {(width/2)-(rectangleWidth/2)};
   int rectanglePosY {height-rectangleHeight};
+  int rectangleYVelocity {0}; // when jumping each frame will decrease by jumpVelo, when falling increase by gravity 
+  const int jumpVelocity {-22}; // acceleration (22p/f)/f
+  const int gravity {1}; // acceleration
+  bool isInAir {}; //false , to prevent air jumping
 
 
 
@@ -26,6 +30,29 @@ int main ()
 
     //rectangle drawing
     DrawRectangle(rectanglePosX, rectanglePosY, rectangleWidth, rectangleHeight , BLUE);
+
+    // jumping logic
+
+    // check is rectangle on ground
+    if (rectanglePosY >= height-rectangleHeight)
+    {
+      isInAir = false;
+      rectangleYVelocity = 0;
+    }
+    else
+    {
+      // rectangle in the air , it will comback to the ground by gravity
+      isInAir = true;
+      rectangleYVelocity += gravity;
+    }
+
+    // jump logic
+    if(IsKeyPressed(KEY_SPACE) && !isInAir)
+    {
+      rectangleYVelocity += jumpVelocity;
+    }
+
+    rectanglePosY += rectangleYVelocity;
 
     EndDrawing();
 
